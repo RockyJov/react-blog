285a5f5f4646c5c9efd5afa18f3c573386613133
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-09-08 02:41:29 +0300

    Update ArticleRate.js
    Firebase functions!

--- src/Layout/ArticleRate/ArticleRate.js
+++ src/Layout/ArticleRate/ArticleRate.js
@@ -4,72 +4,13 @@ import classes from "./ArticleRate.module.css";
 import { Button, Container } from "reactstrap";
 import { isEmpty } from "react-redux-firebase";
 
-const db = firebase.firestore();
-const increment = firebase.firestore.FieldValue.increment(1);
-const decrement = firebase.firestore.FieldValue.increment(-1);
-
 class ArticleRate extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      createUserID: null,
       visible: true,
-      rating: null,
     };
   }
-
-  componentDidMount() {
-    const aid = this.props.location.pathname.slice(9);
-
-    const ref = db.collection("Articles").doc(aid);
-    ref.get().then((doc) => {
-      if (doc.exists) {
-        this.setState({
-          rating: doc.data().count,
-        });
-      } else {
-        console.log("No such doc exists!");
-      }
-    });
-  }
-
-  increment = async () => {
-    const aid = this.props.location.pathname.slice(9);
-
-    const ref = db.collection("Articles").doc(aid);
-    await ref.update({ count: increment });
-    ref.get().then((doc) => {
-      if (doc.exists) {
-        this.setState({
-          rating: doc.data().count,
-          createUserID: doc.data().createUserID,
-          visible: false,
-        });
-        console.log(this.state);
-      } else {
-        console.log("No such doc exists!");
-      }
-    });
-  };
-
-  decrement = async () => {
-    const aid = this.props.location.pathname.slice(9);
-
-    const ref = db.collection("Articles").doc(aid);
-    await ref.update({ count: decrement });
-    ref.get().then((doc) => {
-      if (doc.exists) {
-        this.setState({
-          rating: doc.data().count,
-          createUserID: doc.data().createUserID,
-          visible: false,
-        });
-        console.log(this.state);
-      } else {
-        console.log("No such doc exists!");
-      }
-    });
-  };
 
   render() {
     return (
@@ -79,11 +20,10 @@ class ArticleRate extends Component {
           {this.state.visible ? (
             <div className={classes.Button}>
               {" "}
-              <Button onClick={(e) => this.increment()}>+</Button>
-              <Button onClick={(e) => this.decrement()}>-</Button>
+              <Button>+</Button>
+              <Button>-</Button>
             </div>
           ) : null}
-          <h2>{this.state.rating}</h2>
         </div>
       </Container>
     );
