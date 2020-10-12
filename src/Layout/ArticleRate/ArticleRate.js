396cbf4167611edb44a1f25a516d23517ced3c14
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-10-12 20:00:11 +0300

    rate button conditions

--- src/Layout/ArticleRate/ArticleRate.js
+++ src/Layout/ArticleRate/ArticleRate.js
@@ -9,15 +9,44 @@ const db = firebase.firestore();
 class ArticleRate extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      hasRatedPositive: false,
+      hasRatedNegative: false,
+    };
   }
 
-  // upvoteArticles = (id) => {
-  //   const upvote = firebase.functions().httpsCallable("upvote");
-  //   upvote({ id: id }).catch((error) => {
-  //     console.log(error.message);
-  //   });
-  // };
+  componentDidMount() {
+    const userRef = db.collection("Users").doc(this.props.auth.uid);
+    userRef.get().then((doc) => {
+      //check if user has voted
+      if (
+        doc
+          .data()
+          .positiveRatings.includes(this.props.location.pathname.slice(9))
+      ) {
+        this.setState({ hasRatedPositive: true });
+        console.log(
+          "User has already rated. hasRatedPositive is set to: " +
+            this.state.hasRatedPositive
+        );
+      } else {
+        console.log("user had not yt rated positive");
+      }
+      if (
+        doc
+          .data()
+          .negativeRatings.includes(this.props.location.pathname.slice(9))
+      ) {
+        this.setState({ hasRatedNegative: true });
+        console.log(
+          "User has already rated. hasRatedNegative is set to: " +
+            this.state.hasRatedNegative
+        );
+      } else {
+        console.log("user has not yet rated negative");
+      }
+    });
+  }
 
   positiveRating = () => {
     //get user and article refs
@@ -36,7 +65,11 @@ class ArticleRate extends Component {
           .data()
           .negativeRatings.includes(this.props.location.pathname.slice(9))
       ) {
-        console.log("only rate somthing once!");
+        this.setState({ hasRatedPositive: true });
+        console.log(
+          "only rate somthing once!, hasRatedPositive is set to: " +
+            this.state.hasRatedPositive
+        );
       }
 
       //update voted articles array
@@ -74,7 +107,11 @@ class ArticleRate extends Component {
           .data()
           .negativeRatings.includes(this.props.location.pathname.slice(9))
       ) {
-        console.log("only rate somthing once!");
+        this.setState({ hasRatedNegative: true });
+        console.log(
+          "only rate somthing once!, hasRatedNegative is set to: " +
+            this.state.hasRatedNegative
+        );
       }
 
       //update voted articles array
@@ -100,33 +137,64 @@ class ArticleRate extends Component {
     return (
       <Container style={{ paddingLeft: 0, paddingRight: 0 }}>
         <div className={classes.Button}>
-          <Button
-            style={{
-              fontFamily: "monospace",
-              borderRadius: 0,
-              fontSize: "14px",
-              width: "6rem",
-              marginRight: "1rem",
-            }}
-            outline
-            color="success"
-            onClick={() => this.positiveRating()}
-          >
-            <srong>GOOD</srong>
-          </Button>
-          <Button
-            style={{
-              fontFamily: "monospace",
-              borderRadius: 0,
-              fontSize: "14px",
-              width: "6rem",
-            }}
-            outline
-            color="danger"
-            onClick={() => this.negativeRating()}
-          >
-            <strong>UNGOOD</strong>
-          </Button>
+          {this.state.hasRatedPositive ? (
+            <Button
+              style={{
+                fontFamily: "monospace",
+                borderRadius: 0,
+                fontSize: "14px",
+                width: "6rem",
+                marginRight: "1rem",
+              }}
+              color="success"
+              disabled
+            >
+              <srong>GOOD</srong>
+            </Button>
+          ) : (
+            <Button
+              style={{
+                fontFamily: "monospace",
+                borderRadius: 0,
+                fontSize: "14px",
+                width: "6rem",
+                marginRight: "1rem",
+              }}
+              outline
+              color="success"
+              onClick={() => this.positiveRating()}
+            >
+              <srong>GOOD</srong>
+            </Button>
+          )}
+          {this.state.hasRatedNegative ? (
+            <Button
+              style={{
+                fontFamily: "monospace",
+                borderRadius: 0,
+                fontSize: "14px",
+                width: "6rem",
+              }}
+              color="danger"
+              disabled
+            >
+              <strong>UNGOOD</strong>
+            </Button>
+          ) : (
+            <Button
+              style={{
+                fontFamily: "monospace",
+                borderRadius: 0,
+                fontSize: "14px",
+                width: "6rem",
+              }}
+              outline
+              color="danger"
+              onClick={() => this.negativeRating()}
+            >
+              <strong>UNGOOD</strong>
+            </Button>
+          )}
         </div>
       </Container>
     );
