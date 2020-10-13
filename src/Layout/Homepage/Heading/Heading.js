a51fab854c67a252d3337d5a5a75964390902bc4
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-10-14 01:46:03 +0300

    Navbar loading
    Follows isLoaded condotion when initial load

--- src/Layout/Homepage/Heading/Heading.js
+++ src/Layout/Homepage/Heading/Heading.js
@@ -16,14 +16,13 @@ import {
 import classes from "./Heading.module.css";
 import { connect } from "react-redux";
 import firebase from "../../../Config/firebase";
-import { Link, useLocation } from "react-router-dom";
-import NewComment from "../../../NewComment/NewComment";
 // kokia sios klases paskirtis? Naudoju kelis reactstrap componentus headeri. Collapse componentas turi isOpen property, kuri default bus
 // false. naudojant toggle metoda, paspaudus atnaujinam isOpen property is false i true, kodel? nezinau
 class Heading extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      //navbar  toggler
       isOpen: false,
     };
   }
@@ -58,78 +57,81 @@ class Heading extends Component {
   render() {
     return (
       <div>
-        <Navbar
-          className={classes.Navbar}
-          expand="xs"
-          color="light"
-          fixed="top"
-        >
-          <NavbarBrand style={{ marginRight: 0 }} href="/">
-            {" "}
-            <img
-              className={classes.Logo}
-              src="https://firebasestorage.googleapis.com/v0/b/react-blog-a39d2.appspot.com/o/LogoMakr_85Wfsy.png?alt=media&token=46c44cf2-16d2-4636-ac42-1e4ef459c9c1"
-            />
-          </NavbarBrand>
-          <Nav cla ssName="mr-auto" navbar>
-            {this.props.auth.isEmpty ? (
-              <NavItem>
-                <Button
-                  style={{ borderRadius: 0, fontFamily: "monospace" }}
-                  outline
-                  color="dark"
-                  size="sm"
-                  onClick={() => this.login()}
-                >
-                  <strong>CREATE POST</strong>
-                </Button>
-              </NavItem>
-            ) : (
-              <NavItem>
-                <Button
-                  style={{ borderRadius: 0, fontFamily: "monospace" }}
-                  outline
-                  color="dark"
-                  size="sm"
-                  href="/new-article"
-                >
-                  <strong>CREATE POST</strong>
-                </Button>
-              </NavItem>
-            )}
-          </Nav>
-          <NavbarToggler onClick={this.toggle}></NavbarToggler>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar className="ml-auto">
+        {this.props.auth.isLoaded && (
+          <Navbar
+            className={classes.Navbar}
+            expand="xs"
+            color="light"
+            fixed="top"
+          >
+            <NavbarBrand style={{ marginRight: 0 }} href="/">
+              {" "}
+              <img
+                className={classes.Logo}
+                src="https://firebasestorage.googleapis.com/v0/b/react-blog-a39d2.appspot.com/o/LogoMakr_85Wfsy.png?alt=media&token=46c44cf2-16d2-4636-ac42-1e4ef459c9c1"
+              />
+            </NavbarBrand>
+            <Nav cla ssName="mr-auto" navbar>
               {this.props.auth.isEmpty ? (
                 <NavItem>
-                  {" "}
                   <Button
                     style={{ borderRadius: 0, fontFamily: "monospace" }}
                     outline
                     color="dark"
                     size="sm"
+                    onClick={() => this.login()}
                   >
-                    <strong>INFO</strong>
+                    <strong>CREATE ID</strong>
                   </Button>
                 </NavItem>
               ) : (
                 <NavItem>
-                  <Badge> Logged in as: {this.props.auth.uid}</Badge>{" "}
                   <Button
                     style={{ borderRadius: 0, fontFamily: "monospace" }}
                     outline
                     color="dark"
                     size="sm"
-                    onClick={() => firebase.auth().signOut()}
+                    href="/new-article"
                   >
-                    <strong>LOGOUT</strong>
+                    <strong>CREATE POST</strong>
                   </Button>
                 </NavItem>
               )}
             </Nav>
-          </Collapse>
-        </Navbar>
+
+            <NavbarToggler onClick={this.toggle}></NavbarToggler>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav navbar className="ml-auto">
+                {this.props.auth.isEmpty ? (
+                  <NavItem>
+                    {" "}
+                    <Button
+                      style={{ borderRadius: 0, fontFamily: "monospace" }}
+                      outline
+                      color="dark"
+                      size="sm"
+                    >
+                      <strong>INFO</strong>
+                    </Button>
+                  </NavItem>
+                ) : (
+                  <NavItem>
+                    <Badge> Logged in as: {this.props.auth.uid}</Badge>{" "}
+                    <Button
+                      style={{ borderRadius: 0, fontFamily: "monospace" }}
+                      outline
+                      color="dark"
+                      size="sm"
+                      onClick={() => firebase.auth().signOut()}
+                    >
+                      <strong>LOGOUT</strong>
+                    </Button>
+                  </NavItem>
+                )}
+              </Nav>
+            </Collapse>
+          </Navbar>
+        )}
       </div>
     );
   }
